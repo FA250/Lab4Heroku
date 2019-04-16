2ae52e40b1cb8548a271dc889745dece53757e9d
Unknown <faph.2009@hotmail.com>
2019-04-15 21:47:25 -0600

    Se agregan fragmentos para las listas de autores y libros

--- app/src/main/java/com/example/lab4heroku/MainActivity.java
+++ app/src/main/java/com/example/lab4heroku/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.lab4heroku;
 
+import android.support.v4.app.FragmentTabHost;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private FragmentTabHost mTabHost;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTabHost = (FragmentTabHost) findViewById(android.R.id.tabhost);
+        mTabHost.setup(this, getSupportFragmentManager(), R.id.contenido_tab);
+
+        mTabHost.addTab(mTabHost.newTabSpec("Libros").setIndicator("Libros"),
+                fLibros.class, null);
+        mTabHost.addTab(mTabHost.newTabSpec("Autores").setIndicator("Autores"),
+                fAutores.class, null);
+
     }
 }

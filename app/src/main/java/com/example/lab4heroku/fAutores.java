288de7418b806d5fbc57984e9171a8518811219f
Unknown <faph.2009@hotmail.com>
2019-04-16 23:23:40 -0600

    Se muestra la lista de libros y autores de heroku en la app

--- app/src/main/java/com/example/lab4heroku/fAutores.java
+++ app/src/main/java/com/example/lab4heroku/fAutores.java
@@ -3,70 +3,88 @@ package com.example.lab4heroku;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- *
- * to handle interaction events.
- * Use the {@link fAutores#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
 public class fAutores extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+    private View rootView;
 
 
     public fAutores() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment fAutores.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static fAutores newInstance(String param1, String param2) {
-        fAutores fragment = new fAutores();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+    private void Actualizar_Datos() {
+        try {
+            Conexion conexion = new Conexion();
+
+            String result1 = conexion.execute("https://hidden-thicket-64919.herokuapp.com/authors.json", "GET").get();
+            JSONArray autoresJson = new JSONArray(result1);
+
+            Actualizar_Lista(autoresJson);
+        } catch (InterruptedException e) {
+            Toast.makeText(this.getContext(), e.toString(), Toast.LENGTH_SHORT).show();
+        } catch (ExecutionException e) {
+            Toast.makeText(this.getContext(), e.toString(), Toast.LENGTH_SHORT).show();
+        } catch (JSONException e) {
+            Toast.makeText(this.getContext(), e.toString(), Toast.LENGTH_SHORT).show();
         }
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_autores, container, false);
+    private void Actualizar_Lista(JSONArray autoresJson) {
+        try {
+            if (autoresJson != null) {
+                if (autoresJson.length() > 0) {
+
+                    final List<String> autores = new ArrayList<>();
+
+                    JSONObject elemento, elemento2;
+                    for (int i = 0; i < autoresJson.length(); i++) {
+                        elemento = autoresJson.getJSONObject(i);
+
+                        autores.add(elemento.getString("name")+" "+elemento.getString("lastname"));
+                    }
+
+
+
+                    ArrayAdapter adapter = new ArrayAdapter(getContext(), android.R.layout.simple_list_item_1, android.R.id.text1, autores);
+
+
+                    ListView listaLibros=rootView.findViewById(R.id.listaAutores);
+
+                    listaLibros.setAdapter(adapter);
+
+                } else {
+                    Toast.makeText(getContext(), "Ocurrio un error al mostrar los libros", Toast.LENGTH_LONG).show();
+                }
+            }
+        } catch (Exception e) {
+            Toast.makeText(getContext(), "Ocurrio un error al mostrar los libros", Toast.LENGTH_LONG).show();
+        }
     }
 
-
-
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        rootView= inflater.inflate(R.layout.fragment_autores, container, false);
+        Actualizar_Datos();
+        return rootView;
+    }
 }
--- app/src/main/java/com/example/lab4heroku/fLibros.java
+++ app/src/main/java/com/example/lab4heroku/fLibros.java
@@ -3,70 +3,118 @@ package com.example.lab4heroku;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- *
- * to handle interaction events.
- * Use the {@link fLibros#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class fLibros extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-
+    private View rootView;
 
     public fLibros() {
-        // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment fLibros.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static fLibros newInstance(String param1, String param2) {
-        fLibros fragment = new fLibros();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private void Actualizar_Datos() {
+        try {
+            Conexion conexion = new Conexion();
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            String result = conexion.execute("https://hidden-thicket-64919.herokuapp.com/books.json", "GET").get();
+            JSONArray librosJson = new JSONArray(result);
+
+            conexion = new Conexion();
+            String result1 = conexion.execute("https://hidden-thicket-64919.herokuapp.com/authors.json", "GET").get();
+            JSONArray autoresJson = new JSONArray(result1);
+
+            Actualizar_Lista(librosJson, autoresJson);
+        } catch (InterruptedException e) {
+            Toast.makeText(this.getContext(), e.toString(), Toast.LENGTH_SHORT).show();
+        } catch (ExecutionException e) {
+            Toast.makeText(this.getContext(), e.toString(), Toast.LENGTH_SHORT).show();
+        } catch (JSONException e) {
+            Toast.makeText(this.getContext(), e.toString(), Toast.LENGTH_SHORT).show();
         }
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_libros, container, false);
+    private void Actualizar_Lista(JSONArray librosJson,JSONArray autoresJson) {
+        try {
+            if (librosJson != null) {
+                if (librosJson.length() > 0) {
+
+                    final List<String> nombres = new ArrayList<>();
+                    final List<String> autores = new ArrayList<>();
+
+                    JSONObject elemento, elemento2;
+                    for (int i = 0; i < librosJson.length(); i++) {
+                        elemento = librosJson.getJSONObject(i);
+
+                        nombres.add(elemento.getString("title"));
+
+                        boolean existeAutor=false;
+                        for(int j = 0;j<autoresJson.length();j++){
+                            elemento2=autoresJson.getJSONObject(j);
+                            if(elemento2.getString("id").equals(elemento.getString("author_id"))){
+                                existeAutor=true;
+                                autores.add(elemento2.getString("name")+" "+elemento2.getString("lastname"));
+                                j=autoresJson.length();
+                            }
+                        }
+
+                        if(!existeAutor)
+                            autores.add("");
+                    }
+
+
+
+                    ArrayAdapter adapter = new ArrayAdapter(getContext(), android.R.layout.simple_list_item_2, android.R.id.text1, nombres) {
+                        @Override
+                        public View getView(int position, View convertView, ViewGroup parent) {
+                            View view = super.getView(position, convertView, parent);
+                            TextView text1 = (TextView) view.findViewById(android.R.id.text1);
+                            TextView text2 = (TextView) view.findViewById(android.R.id.text2);
+
+                            text1.setText(nombres.get(position).toString());
+                            text2.setText(autores.get(position).toString());
+                            return view;
+                        }
+                    };
+
+
+                    ListView listaLibros=rootView.findViewById(R.id.listaLibros);
+
+                    listaLibros.setAdapter(adapter);
+
+                } else {
+                    Toast.makeText(getContext(), "Ocurrio un error al mostrar los libros", Toast.LENGTH_LONG).show();
+                }
+            }
+        } catch (Exception e) {
+            Toast.makeText(getContext(), "Ocurrio un error al mostrar los libros", Toast.LENGTH_LONG).show();
+        }
     }
 
-
-
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        rootView= inflater.inflate(R.layout.fragment_libros, container, false);
+        Actualizar_Datos();
+        return rootView;
+    }
 }
